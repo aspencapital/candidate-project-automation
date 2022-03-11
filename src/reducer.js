544ea746249554a1954f8244f6cf8d550cb87dd9
Chris Blain <71914913+cblain1@users.noreply.github.com>
2022-03-10 21:22:11 -0800

    Delete src directory

--- src/reducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { reducer as form } from 'redux-form';
-
-import forecast from './ducks/forecast';
-
-export default combineReducers({
-  form
-, forecast
-});
